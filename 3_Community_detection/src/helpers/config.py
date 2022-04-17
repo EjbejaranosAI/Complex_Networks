41546b554a9369a0bdba61b943f071df778b5162
“Eric <ewalzthony@gmail.com>
2022-04-17 16:42:51 +0200

    cleaned dataloading and plotting

--- 3_Community_detection/src/helpers/config.py
+++ 3_Community_detection/src/helpers/config.py
@@ -1,9 +1,9 @@
 ## function to make the model information readable, model_type to the list of files corresponding models
-def paths_dict(dir:str='./data') -> dict:
+def config_dict(dir:str='./data') -> dict:
     """Returns a dictionary with the models of each type."""
     model_info = {
-        "MODEL": f"{dir}/model/",
-        "REAL": f"{dir}/real/",
-        "TOY": f"{dir}//toy/",
+        "model": f"{dir}/model",
+        "real": f"{dir}/real",
+        "toy": f"{dir}/toy",
     }
     return model_info
